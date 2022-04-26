513d7da92c36856996a950f79a9ea6e7d6f3a9af
Denis Kulakovskiy <70133789+denforplay@users.noreply.github.com>
2022-04-26 09:57:57 +0300

    Add modificators tests

---
+++ src/Invasion.Tests/ModificatorsTests.cs
@@ -0,0 +1,45 @@
+﻿using Invasion.Engine;
+using Invasion.Engine.Graphics;
+using Invasion.Models.Factories;
+using Invasion.Models.Factories.ModificatorFactories;
+using Invasion.Models.Modificators;
+using Xunit;
+
+namespace Invasion.Tests
+{
+    public class ModificatorsTests
+    {
+        private IModelFactory<ModificatorBase> _modificatorsFactory;
+        private IGraphicProvider _graphicProvider;
+
+        public ModificatorsTests()
+        {
+            _graphicProvider = new DirectXGraphicsProvider(new SharpDX.Windows.RenderForm());
+            Compose();
+        }
+
+        [Fact]
+        public void SpeedBonus_ApplyTest()
+        {
+            Player player = new Player(null, new PlayerConfiguration(1, 10));
+            _modificatorsFactory = new SpeedBonusFactory(_graphicProvider, null);
+            var bonus = _modificatorsFactory.Create();
+            bonus.Apply()
+        }
+
+        public void Compose()
+        {
+            _graphicProvider.LoadBitmap(@"Sources\background.bmp");
+            _graphicProvider.LoadBitmap(@"Sources\character.png");
+            _graphicProvider.LoadBitmap(@"Sources\topdownwall.png");
+            _graphicProvider.LoadBitmap(@"Sources\pistol.png");
+            _graphicProvider.LoadBitmap(@"Sources\knife.png");
+            _graphicProvider.LoadBitmap(@"Sources\defaultBullet.png");
+            _graphicProvider.LoadBitmap(@"Sources\shootingEnemy.png");
+            _graphicProvider.LoadBitmap(@"Sources\speedBonus.png");
+            _graphicProvider.LoadBitmap(@"Sources\slowTrap.png");
+            _graphicProvider.LoadBitmap(@"Sources\kamikadzeEnemy.png");
+            _graphicProvider.LoadBitmap(@"Sources\beatingEnemy.png");
+        }
+    }
+}
